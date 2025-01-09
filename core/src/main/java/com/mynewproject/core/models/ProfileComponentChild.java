9d16183df5a5cb47f6f18d933c7177116b70bc31
unknown <ganesh@employdemy.com>
2025-01-09 18:25:02 +0530

    Added Sling model for profile Component

--- core/src/main/java/com/mynewproject/core/models/ProfileComponentChild.java
+++ core/src/main/java/com/mynewproject/core/models/ProfileComponentChild.java
@@ -3,12 +3,14 @@ package com.mynewproject.core.models;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
 import lombok.Getter;
 
 @Getter
 @Model(adaptables = Resource.class,defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class ProfileComponentChild {
+    @ValueMapValue
     private String text;
 
 }
